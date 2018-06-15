fe5b1568c55f634aec11b03b42a8a11717b9c7cf
lijianguo1211 <risesli@hotmail.com>
2018-06-16 00:29:55 +0800

    '模型‘

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $table = 'user';
+    //开启时间戳自动写入
+    public $timestamps = true;
+    //调整时间戳字段
+    const CREATED_AT = '';
+    const UPDATED_AT = '';
+    //添加数据白名单
+    protected $fillable = [];
 }
