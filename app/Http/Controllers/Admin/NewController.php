dee57776b80f8177843fc7fad4c3100a08ba9900
lijianguo <1539853340@qq.com>
2018-06-19 15:31:40 +0800

    '360新闻查询成功'

--- app/Http/Controllers/Admin/NewController.php
+++ app/Http/Controllers/Admin/NewController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class NewController extends Controller
+class NewController extends BaseController
 {
     public function index()
     {
@@ -14,8 +14,9 @@ class NewController extends Controller
 
     public function search(Request $request)
     {
-        $kw = trim($request->get('title'));var_dump($kw);
+        $kw = trim($request->get('title'));//var_dump($kw);
         $kw = urlencode($kw);
+        //var_dump($kw);exit;
         if(empty($kw)) {
             $data = ['status'=>0,'msg'=>'请输入关键词查询'];
             $this->ajaxReturn($data);
@@ -23,6 +24,13 @@ class NewController extends Controller
         $apikey = 'BZIaJZmLL506tj6UE2XSiBTwIBtinQrTODCmsLqB2BuXmqbMb8g4eGngwcHQP79I';
         $url = "http://api01.bitspaceman.com:8000/news/qihoo?apikey=".$apikey.'&kw='.$kw;
         $jsonArr = $this->getcurl($url);
-        dump($jsonArr);
+        //var_dump($jsonArr);先判断是否查询成功
+        $arr = json_decode($jsonArr,true);
+        if($arr['retcode'] != '000000') {
+            $data = ['status'=>0,'msg'=>'查询失败'];
+            $this->ajaxReturn($data);
+        }
+        $data = ['status'=>1,'msg'=>'查询成功','list'=>$jsonArr];
+        $this->ajaxReturn($data);
     }
 }
--- resources/views/admin/new/index.blade.php
+++ resources/views/admin/new/index.blade.php
@@ -11,12 +11,16 @@
                 <div class="layui-card-header">
                     <form class="layui-form" id="form">
                          <input type="text" id="kw" name="title" class="layui-input">
+                        @csrf
                         <a class="layui-btn layui-btn-primary layui-btn-radius" id="btns">搜索</a>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+        <div style="padding: 40px;" id="news">
+            {{--<div class="layui-col-sm12 layui-col-md9"><div class="layui-card"><div class="layui-card-header">标题</div><div class="layui-card-body">内容</div><div class="layui-card-header">作者</div></div></div>--}}
+        </div>
     </div>
 @endsection
 @section('js')
@@ -28,7 +32,26 @@
                 type:"post",
                 data:$("#form").serialize(),
                    success:function(res) {
-                    console.log(res);
+                    //console.log(res);////console.log(obj.status);//console.log(obj.msg);
+                    var obj = JSON.parse(res);
+                       //console.log(obj);
+                       if(obj.status ==1) {
+                         //查询成功
+                           layer.msg( obj.msg, {icon: 1});
+                         //显示出查询出来的内容
+                           var data = JSON.parse(obj.list);
+                           console.log(data.data);
+                           console.log(data.data[0].title);
+                           var strHtml = '';
+                           $.each(data.data,function(index,value){
+                               var str = "<div class=\"layui-col-sm12 layui-col-md9\"><div class=\"layui-card\"><div class=\"layui-card-header\"><a href="+value.url+">"+value.title+"</a></div><div class=\"layui-card-body\">"+value.content+"</div><div class=\"layui-card-header\">"+'作者:'+value.posterScreenName+'  时间:'+value.publishDate+"</div></div></div>";
+                               strHtml += str;
+                           });
+                           $("#news").append().html(strHtml);
+                       } else {
+                         //查询失败
+                           layer.msg( obj.msg, {icon: 2});
+                       }
                    }
                });
            });
