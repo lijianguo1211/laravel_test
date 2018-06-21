133db46d9c0723611159097d14561a8fbd3d5c81
lijianguo <1539853340@qq.com>
2018-06-21 20:21:00 +0800

    '添加'

--- app/Http/Controllers/Admin/BaiduController.php
+++ app/Http/Controllers/Admin/BaiduController.php
@@ -33,18 +33,69 @@ class BaiduController extends BaseController
             $this->ajaxReturn(['status'=>0,'msg'=>'缺少必传参数']);
         }
         $AppID = '11426448';
-        //$ApiKey = 'StbsLBfHfPwLUOafmopLOMPZ';
-        //$SecretKey = 'kUKK1GGA8jkBzF2r8kdDzTaqyxPDmAOR';
-        //$url1 = 'https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id='.$ApiKey.'&client_secret='.$SecretKey;
-        //$result1 = $this->getCurlHttps($url1);
-        //$result2 = json_decode($result1,true);
+        $ApiKey = 'StbsLBfHfPwLUOafmopLOMPZ';
+        $SecretKey = 'kUKK1GGA8jkBzF2r8kdDzTaqyxPDmAOR';
+        $url1 = 'https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id='.$ApiKey.'&client_secret='.$SecretKey;
+        $result1 = $this->getCurlHttps($url1);
+        $result2 = json_decode($result1,true);
         //dump($result2['access_token']);exit;
-        /*if(empty($result2['access_token'])) {
-            $this->ajaxReturn(['status'=>0,'msg'=>'获取token失败']);
-        }*/
+        $token = $result1['access_token'];
         $test = urlencode($test);
+        #发音人选择, 0为普通女声，1为普通男生，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女声
+        $per = 4;
+        #语速，取值0-9，默认为5中语速
+        $spd = 5;
+        #音调，取值0-9，默认为5中语调
+        $pit = 5;
+        #音量，取值0-9，默认为5中音量
+        $vol = 5;
         $userID = 'c1a2ee5de28a4b6b8769cdb2a5f4e4e5';
-        $url2 = 'http://tsn.baidu.com/text2audio?tex='.$test.'&lan=zh&cuid='.$userID.'&ctp=1&tok=24.cb7dbce8af35fd0f4f70b1b85dd48f0c.2592000.1532162333.282335-11426448';
-        $result3 = $this->getcurl($url2);
+        //$cuid = "123456PHP";
+        $params = array(
+            'tex' => $test,
+            'per' => $per,
+            'spd' => $spd,
+            'pit' => $pit,
+            'vol' => $vol,
+            'cuid' => $userID,
+            'tok' => $token,
+            'lan' => 'zh', //固定参数
+            'ctp' => 1, // 固定参数
+        );
+        $url2 = 'http://tsn.baidu.com/text2audio?' . http_build_query($params);
+        $g_has_error = true;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url2);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+
+        function read_header($ch, $header){
+            global $g_has_error;
+
+            $comps = explode(":", $header);
+            // 正常返回的头部 Content-Type: audio/mp3
+            // 有错误的如 Content-Type: application/json
+            if (count($comps) >= 2){
+                if (strcasecmp(trim($comps[0]), "Content-Type") == 0){
+                    if (strpos($comps[1], "mp3") > 0 ){
+                        $g_has_error = false;
+                    }else{
+                        echo $header ." , has error \n";
+                    }
+                }
+            }
+            return strlen($header);
+        }
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION, 'read_header');
+        $data = curl_exec($ch);
+        if(curl_errno($ch))
+        {
+            echo curl_error($ch);
+            exit;
+        }
+        curl_close($ch);
+
+        $file = $g_has_error ? "result.txt" : "result.mp3";
+        file_put_contents($file, $result3);
     }
 }
