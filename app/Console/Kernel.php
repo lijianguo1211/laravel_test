e4954ff7ad0ecf3aab51cd12267cdf2faa27143c
lijianguo1211 <risesli@hotmail.com>
2018-08-26 19:04:55 +0800

    '调度任务修改‘

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,11 +25,23 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+       // * * * * * /usr/local/bin/php /data/wwwroot/default/laravel_test/artisan schedule:run >> /dev/null 2>&1
 //         $schedule->command('inspire')
 //                  ->hourly();
         //$filePath = __DIR__.mt_rand(0,1000);->sendOutputTo($filePath)
         $schedule->call(function(){
-            DB::table('ui_user')->count();
+            $data = [
+                'username'   => 'TSET_'.mt_rand(1000,9999),
+                'sex'        => mt_rand(0,1),
+                'age'        => mt_rand(10,99),
+                'class'      => '三年级'.mt_rand(1,10).'班',
+                'bobby'      => '打球，写字，散步'.mt_rand(0,10000),
+                'email'      => '15398533'.mt_rand(10,99).'@qq.com',
+                'mobile'     => '15971896'.mt_rand(100,999),
+                'updatetime' => time(),
+                'createtime' => time(),
+            ];
+            DB::table('ui_test')->insert($data);
         })->everyMinute();
     }
 
