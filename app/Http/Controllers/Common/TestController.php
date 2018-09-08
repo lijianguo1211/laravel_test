e9d0ed72999e677d9019ff09877639736feada66
lijianguo1211 <risesli@hotmail.com>
2018-09-08 15:58:34 +0800

    tree

--- app/Http/Controllers/Common/TestController.php
+++ app/Http/Controllers/Common/TestController.php
@@ -13,9 +13,9 @@ class TestController
     {
         $test = new CreateTreeRootController(0,1);
         $test->AddNode(3,0,0);
-        $test->AddNode(4,1,0);
+        /*$test->AddNode(4,1,0);
         $test->AddNode('A',1,3);
-        $test->AddNode('B',0,4);
+        $test->AddNode('B',0,4);*/
         dd($test);
         dd($test->PreOrderTraversal());
         dd($test->InOrderTraversal());
